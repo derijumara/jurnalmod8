1f1abdb524ca7fdad0a1e365923915066228ae98
derijumara <derijumara97@gmail.com>
2017-04-07 10:25:53 +0700

    Add files via upload

---
+++ faktorial.php
@@ -0,0 +1,16 @@
+<?php
+  echo"<table align='center'><tr><td align='center'>";
+    for($i=0;$i<=3;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('*');
+       }
+    echo"<br/>";
+   }
+  for($i=0;$i<=2;$i++){
+    for($a=$i;$a<=2;$a++){
+      print('*');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+?>
